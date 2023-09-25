da7b3e7fc6301511de7dc0b0c0e530d1942b1039
Stefan Ille <stefan.ille@designa.com>
2023-09-25 13:33:18 +0200

    Exercise 1.14* (anecdotes step3)

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -6,9 +6,13 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const DisplayAnecdoteVotes = ({ votes }) => {
+// Display the anecdote and the number of votes
+const DisplayAnecdote = ({ anecdote, votes }) => {
   return (
-    <p>has {votes} votes</p>
+    <div>
+      <p>{anecdote}</p>
+      <p>has {votes} votes</p>
+    </div>
   )
 }
 
@@ -26,15 +30,19 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   // store the votes of each anecdote in the 'votes' array
-  // create the array with the same length as the anecdotes array and fill it with zeros
+  // create the array with the same length as the anecdotes array 
+  // so we can follow the number of votes for each anecdote and fill it initally with zeros
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [mostVoted, setMostVoted] = useState(0)
 
   const handleNextClick = () => {
     // take the length of the anecdotes array
     const anecdotesLength = anecdotes.length
     // and create a random index between 0 and the length of the anecdotes array
+    // Math.random() returns a number between 0 and 1 sow we have to multiply it 
+    // with the length of the anecdotes array to get the random index of an anecdote
     const randomIndex = Math.floor(Math.random() * anecdotesLength)
-    // set the selected anecdote to this of the random index
+    // set the selected anecdote to the anecdote of the random index
     setSelected(randomIndex)
   }
 
@@ -43,21 +51,28 @@ const App = () => {
     console.log('selected', selected)
     console.log('votes', votes)
     console.log('votes[selected] before ', votes[selected])
-    // first create a copy of the votes array
+    // first create a copy of the votes array 
+    // (remember it's not allowed to manipulate the state directly!)
     const votes_copy = [...votes]
     // then increment the value in the copy
     votes_copy[selected] += 1
     // finally update the state with the copy
     setVotes(votes_copy)
     console.log('votes[selected] after ', votes_copy[selected])
+    // get the index of the anecdote with the most votes using the Math.max() 
+    // function on the votes_copy array
+    const mostVotedIndex = votes_copy.indexOf(Math.max(...votes_copy))
+    console.log('mostVotedIndex', mostVotedIndex)
+    // update the state with the index of the anecdote with the most votes
+    setMostVoted(mostVotedIndex)
   }
 
   return (
     <div>
-      <div>
-        {anecdotes[selected]}
-        <DisplayAnecdoteVotes votes={votes[selected]} />
-      </div>
+      <h1>Anecdote of the day</h1>
+      <DisplayAnecdote anecdote={anecdotes[selected]} votes={votes[selected]} />
+      <h1>Anecdote with most votes</h1>
+      <DisplayAnecdote anecdote={anecdotes[mostVoted]} votes={votes[mostVoted]} />
       <div>
         <Button handleClick={handleVoteClick} text='vote'/>
         <Button handleClick={handleNextClick} text='next anecdote'/>
