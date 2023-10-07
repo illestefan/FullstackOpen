86e8231ff608aaa403c1209123c558e97bbd7136
Stefan Ille <github@ille-net.de>
2023-10-07 18:00:49 +0200

    added mongo db access

--- part3-phonebook/phonebook-backend/index.js
+++ part3-phonebook/phonebook-backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan');
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(cors())
@@ -17,41 +18,14 @@ morgan.token('body', function (req) {
 // POST /api/persons 200 71 - 3.603 ms {"name":"Mary Jane Poppins","number":"0039 3876-353435666"}
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-// generate a random id between 1 and 1000000 for a new person
-function generateId() {
-    const id = Math.floor(Math.random() * 1000000)
-    return id
-}
-
 // info route
 app.get('/info', (request, response) => {
     const date = new Date()
+    const personsCount = Person.length
+    console.log('count: ', personsCount)
     response.send(
         `
-        <p>Phonebook has info for ${persons.length} people</p>
+        <p>Phonebook has info for ${personsCount} people</p>
         <p>${date}</p>
         `
     )
@@ -59,45 +33,46 @@ app.get('/info', (request, response) => {
 
 // get all persons route
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        console.log('got persons', persons)
+        response.json(persons)
+    })
 })
 
 // get a single person route
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     console.log(id)
-    const person = persons.find(person => {
-        return person.id === id
-    })
-    console.log(person)
-    if (person) {
+    Person.findById(request.params.id)
+    .then(person => {
+        console.log('found person: ', person)
         response.json(person)
-    } else {
+    })
+    .catch(error => {
+        console.log(error)
         response.statusMessage = `Person with id ${id} not found`
         response.status(404).end()
-    }
+    })
 })
 
 // delete a person route
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => {
-        return person.id === id
-    })
-    if (person) {
-        persons = persons.filter(persons => persons.id !== id)
+    console.log('deleting person with id: ', request.params.id)
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
         response.status(204).end()
-    } else {
+    })
+    .catch(error => {
+        console.log(error)
         response.statusMessage = `Person with id ${id} not found`
         response.status(404).end()
-    }
-    console.log(persons)
+    })
 })
 
 // add a person route
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(body)
+    console.log('adding a person: ', body)
 
     // if name or number is missing, return an error
     if (!body.name) {
@@ -113,28 +88,15 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    // if the name already exists, return an error
-    const personExists = persons.find(person => {
-        return person.name === body.name
-    })
-    if (personExists) {
-        response.statusMessage = 'name must be unique'
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
     // generate a new person object
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-    // add the new person to the persons array
-    persons = persons.concat(person)
-    console.log(person)
-    // return the new person
-    response.json(person)
+        number: body.number
+    })
+    // add the new person to the database
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })  
 })
 
 // 404 middleware for unknown endpoints
