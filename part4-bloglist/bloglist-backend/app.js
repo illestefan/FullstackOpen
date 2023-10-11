ed109839fa14280b0457d1cd15bcc4e3fcd762a3
Stefan Ille <github@ille-net.de>
2023-10-11 15:25:30 +0200

    Exercises 4.1 - 4.2 finished

---
+++ part4-bloglist/bloglist-backend/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const { setupMorgan } = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB!')
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(setupMorgan())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
