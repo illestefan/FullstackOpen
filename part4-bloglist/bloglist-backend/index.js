1c63460dc231099c61829f1831f0fea0279af4e1
Stefan Ille <github@ille-net.de>
2023-10-11 15:05:38 +0200

    refactored step 1

--- part4-bloglist/bloglist-backend/index.js
+++ part4-bloglist/bloglist-backend/index.js
@@ -3,34 +3,23 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const { setupMorgan } = require('./utils/middleware')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    console.log('transforming', returnedObject)
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-console.log('connecting to', mongoUrl)
-mongoose.connect(mongoUrl)
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB!')
+    logger.info('connected to MongoDB!')
   })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(setupMorgan())
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -50,7 +39,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
