04e901f4a8c1be348b5a8213a7a7150c28c99d05
Stefan Ille <github@ille-net.de>
2023-10-11 07:36:01 +0200

    Started Part4 (separate modules)

--- part3-notes/notes-backend/models/note.js
+++ part3-notes/notes-backend/models/note.js
@@ -1,31 +1,16 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-mongoose.set('strictQuery',false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB, result:', result)
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    console.log('transforming', returnedObject)
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
