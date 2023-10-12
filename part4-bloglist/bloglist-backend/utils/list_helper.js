4a48711d84a0a3315b7e92988931123022122ac9
Stefan Ille <github@ille-net.de>
2023-10-12 07:43:29 +0200

    Exercise 4.5*

--- part4-bloglist/bloglist-backend/utils/list_helper.js
+++ part4-bloglist/bloglist-backend/utils/list_helper.js
@@ -15,7 +15,26 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  // return empty object if list of blogs is empty
+  if (blogs.length === 0) {
+    return {}
+  }
+
+  // lay out blogs to a list with blog.likes and find the max
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  // find the blog that has the max likes
+  const favBlog = blogs.find(blog => blog.likes === maxLikes)
+  // return object as specified
+  return ({
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
