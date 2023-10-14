7293ed7294dec01381d823fb3c824013f758e20b
Stefan Ille <github@ille-net.de>
2023-10-14 17:40:47 +0200

    Exercise 4.9 (bloglist tests step2)

--- part4-bloglist/bloglist-backend/tests/blog_api.test.js
+++ part4-bloglist/bloglist-backend/tests/blog_api.test.js
@@ -19,6 +19,11 @@ test('all blogs are returned and blogs are returned as json', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier of blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
